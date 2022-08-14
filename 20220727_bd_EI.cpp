8a1c0ce3e41540275d94a68e4dc34e298a56e054
ella-collins <57517839+ella-collins@users.noreply.github.com>
2022-08-14 20:16:47 +0800

    Add files via upload

--- 20220727_bd_EI.cpp
+++ 20220727_bd_EI.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
+#include<bits/stdc++.h>
 using namespace std;
 
 void bubbleSort(vector<int>& nums){
@@ -30,19 +31,40 @@ vector<int> findIndexInNum2(vector<int>& nums1, vector<int>& nums2){
     return ans;
 }
 
+//20220814更新更优的方法应该是用单调栈,类似LC496,因为LC496上是返回下一个更大的元素值，所以这个题用pair记录索引位置
+vector<int> findNextLargerIndexInNums2(vector<int>& nums1, vector<int>& nums2){
+    int m = nums1.size(), n = nums2.size();
+    unordered_map<int,pair<int,int>> hashmap;
+    stack<pair<int,int>> st;
+    for (int i = n - 1; i >= 0; --i) {
+        int num = nums2[i];
+        while (!st.empty() && num >= st.top().first) {
+            st.pop();
+        }
+        hashmap[num] = st.empty() ? make_pair(-1, -1) : st.top();
+        st.push(make_pair(num, i));
+    }
+    vector<int> res(m);
+    for (int i = 0; i < m; ++i) {
+        res[i] = hashmap[nums1[i]].second;
+    }
+    return res;
+}
+
 
 int main(){
     //Problem1
-    vector<int> num = {9, 5, 7, 3, 2};
-    bubbleSort(num);
-    for(int i=0; i<num.size(); ++i){
-        cout<<num[i]<<endl;
-    }
+//    vector<int> num = {9, 5, 7, 3, 2};
+//    bubbleSort(num);
+//    for(int i=0; i<num.size(); ++i){
+//        cout<<num[i]<<endl;
+//    }
 
     //Problem2
     vector<int> num1 = {9, 5, 7};
     vector<int> num2 = {5, 2, 7, 9, 11, 3, 1};
-    vector<int> ans = findIndexInNum2(num1, num2);
+//    vector<int> ans = findIndexInNum2(num1, num2);
+    vector<int> ans = findNextLargerIndexInNums2(num1, num2);
     for(int i=0; i<ans.size(); ++i){
         cout<<ans[i]<<endl;
     }
